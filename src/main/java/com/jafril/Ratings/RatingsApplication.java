f291b0472f7c2ddecfeafc273fa25d5c6b56ca3b
1707073Jafril <jafrilalamshihab.kuetcse@gmail.com>
2024-10-26 05:34:20 +0600

    File uploaded

---
+++ src/main/java/com/jafril/Ratings/RatingsApplication.java
@@ -0,0 +1,15 @@
+package com.jafril.Ratings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class RatingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingsApplication.class, args);
+	}
+
+
+}
